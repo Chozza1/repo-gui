75c54c0983f9a0efe77d909133941facc1f1f00a
Aleksandr <aleks2102@gmail.com>
2021-02-07 21:54:57 +0300

    Исправления

--- Homework.java
+++ Homework.java
@@ -1,6 +1,5 @@
 public class Homework {
     public static void main(String[] args) {
-        task_8();
         int a = 1;
         double b = 0.1;
         float c = 0.001f;
@@ -8,22 +7,22 @@ public class Homework {
         short e = 12;
         byte f = 127;
         char g = 12;
-        boolean h;
+        boolean h = true;
     }
 
-    public static float task_3() {
-       float a = 0.0002f;
-       float b = 0.00001f;
-       float c = 1.54333f;
-       float d = 1.123123f;
-       float e = a*(b+(c/d));
+    public static float task_3(float a, float b, float c, float d, float e) {
+       a = 0.0002f;
+       b = 0.00001f;
+       c = 1.54333f;
+       d = 1.123123f;
+       e = a*(b+(c/d));
        return e;
     }
 
-    public static boolean task_4() {
-        int a = 2;
-        int b = 7;
-        int c = a + b;
+    public static boolean task_4(int a, int b, int c) {
+        a = 2;
+        b = 7;
+        c = a + b;
         if (c>=10 && c<=20){
             return true;
         }
@@ -32,8 +31,8 @@ public class Homework {
        }
     }
 
-    public static void task_5() {
-        int a = -10;
+    public static void task_5(int a) {
+        a = -10;
         if (a>=0){
             System.out.println(" Число положительное");
         }
@@ -42,8 +41,8 @@ public class Homework {
         }
     }
 
-    public static boolean task_6() {
-        int a = 1;
+    public static boolean task_6(int a) {
+        a = 1;
         if (a <= 0) {
             return true;
         }
@@ -55,8 +54,8 @@ public class Homework {
         System.out.println("Привет" + Мир);
     }
 
-    public static void task_8(){
-        int yaer;
+
+    public static void task_8(int yaer){
         yaer=1968;
         System.out.println();
         if ((yaer%400==0 || yaer%100!=0) &&(yaer%4==0)) {
@@ -65,7 +64,7 @@ public class Homework {
         else {
             System.out.println("Год не високосный");
         }
-        
+
     }
 
 }
